27a81d2f00ff46cf1b9227ff01f2d1ad94342973
Pranav Waghmare <pranavwaghmare010206@gmail.com>
2026-02-28 08:32:11 +0000

    Update ALLOWED_HOSTS to include localhost and all hosts

--- expense_manager/settings.py
+++ expense_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3pg&97b%f9ce)%%d=j6q1*ln$s$j*ls@#^fkfqfnn)0%sv0cvf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '*']
 
 
 # Application definition
